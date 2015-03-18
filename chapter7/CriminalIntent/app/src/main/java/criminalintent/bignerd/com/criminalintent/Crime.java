94aeb94811414c9e085137fca653a4a9dac32126
Zeeshan Hanif <zeeshanhanif@gmail.com>
2015-03-18 22:36:17 +0500

    Chapter 7 inprogress

---
+++ chapter7/CriminalIntent/app/src/main/java/criminalintent/bignerd/com/criminalintent/Crime.java
@@ -0,0 +1,32 @@
+package criminalintent.bignerd.com.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by zeeshan on 3/18/2015.
+ */
+public class Crime {
+
+    private UUID mId;
+    private String mTitle;
+
+    public Crime(){
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public void setId(UUID mId) {
+        this.mId = mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String mTitle) {
+        this.mTitle = mTitle;
+    }
+}
