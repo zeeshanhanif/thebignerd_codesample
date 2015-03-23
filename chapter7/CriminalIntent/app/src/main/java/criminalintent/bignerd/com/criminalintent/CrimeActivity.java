c1629fee24df9bcb8948844d71e9c5cc2200d6df
Zeeshan Hanif <zeeshanhanif@gmail.com>
2015-03-23 22:18:57 +0500

    Chapter 7 inprogress

--- chapter7/CriminalIntent/app/src/main/java/criminalintent/bignerd/com/criminalintent/CrimeActivity.java
+++ chapter7/CriminalIntent/app/src/main/java/criminalintent/bignerd/com/criminalintent/CrimeActivity.java
@@ -1,6 +1,8 @@
 package criminalintent.bignerd.com.criminalintent;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -13,6 +15,16 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+
+        if(fragment == null){
+            fragment = new CrimeFragment();
+            fm.beginTransaction()
+                .add(R.id.fragmentContainer,fragment)
+                .commit();
+        }
     }
 
 }
